7b92b6e724869a4ba61b0fc9fbe4095ae67a7ec7
paras594 <parasarora594@gmail.com>
2018-08-29 12:04:26 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>
+      Hello, My name is Paras Arora. I am from India. I am a front end web
+      developer. :)
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
